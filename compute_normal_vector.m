418134e1855a362a8740ba8d211358352867e4d6
Camille Hamon <camilleh@ntnu.no>
2015-12-08 15:11:46 +0100

    Initial mockup for the main function

---
+++ compute_normal_vector.m
@@ -0,0 +1,10 @@
+function normal = compute_normal_vector(mpc,bif_info,param_list)
+% COMPUTE_NORMAL_VECTOR(MPC,BIF_INFO_PARAM_LIST) computes the NORMAL vector
+% in the space of parameters defined in PARAM_LIST at the operating point 
+% defined in MPC. The operating point is assumed to be either a saddle-node
+% bifurcation point (SNB) or a limit-induced bifurcation point (LIB).
+% Information about the bifurcation is provided in BIF_INFO.
+
+%% Compute normal vector
+
+end
